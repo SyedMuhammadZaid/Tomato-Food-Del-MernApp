1a3105266e8aebf1c5e092c68cc92087c6eb973e
SyedMuhammadZaid <78557583+SyedMuhammadZaid@users.noreply.github.com>
2024-06-01 15:47:12 +0500

    Update StoreContext.jsx

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -11,7 +11,7 @@ const StoreContextProvider = (props) => {
 
   const [token, setToken] = useState("");
   axios.defaults.withCredentials = true;
-  const url = "https://tomato-food-del-backend.vercel.app";
+  const url = "https://tomato-food-del-backendapp.onrender.com";
 
   const getFoodList = async () => {
     let res = await axios.get(`${url}/api/food/list`,{ withCredentials: true });
